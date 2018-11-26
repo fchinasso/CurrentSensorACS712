ac475571e63cca007e2cfa8fa3fb2b5481ff6bc9
Felipe Chinasso <45355530+fchinasso@users.noreply.github.com>
2018-11-26 09:15:17 -0200

    First Commit
    First .h e . cpp from the Arduino Library for the ACS712 currente sensor

---
+++ sensorCorrente.h
@@ -0,0 +1,30 @@
+
+#ifndef sensorCorrente_h
+#define sensorCorrente_h
+
+#include "Arduino.h"
+
+enum sensorCorrente_type {T5B,T20A,T30A};
+
+
+class sensorCorrente{
+    
+    public:
+        sensorCorrente(int _pino,sensorCorrente_type modelo);
+        float calculaRMS();
+        float calculaCorrente(int A);
+        void calibrar(); 
+	   float calculaDigitalIpp();
+        
+
+
+    private:
+        int pino;
+        int fatorConversao;
+        int zero = 512;
+    
+  
+};
+
+
+#endif
