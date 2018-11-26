89f0aaa0121c263af94c49259310a346eecdbe80
Felipe Chinasso <45355530+fchinasso@users.noreply.github.com>
2018-11-26 10:49:54 -0200

    mudanças parametros no master

--- sensorCorrente.cpp
+++ sensorCorrente.cpp
@@ -7,30 +7,30 @@ sensorCorrente::sensorCorrente(int _pino,sensorCorrente_type modelo){     //METO
     pino=_pino;
 
     switch(modelo){
-     
+
       case T5B:
       fatorConversao = 0.185;
       break;
-      
+
       case T20A:
       fatorConversao = 0.100;
       break;
-      
+
       case T30A:
       fatorConversao = 0.066;
       break;
-      
+
     }
 }
 void sensorCorrente::calibrar(){
-    int n =0;
+    int n = 0;
 
     for(int i =0;i<10;i++){
 
-      n+= analogRead(pino);
+      n += analogRead(pino);
     }
      zero = n/10;
-  
+
 }
 float sensorCorrente::calculaDigitalIpp(){
       float periodo = (float) 1/60;
@@ -39,14 +39,14 @@ float sensorCorrente::calculaDigitalIpp(){
       int menor=zero;
       unsigned long inicio = millis();
 
-      while(millis()-inicio < periodo){
+      while(millis()-inicio < 10 * periodo){
         x=analogRead(pino)-zero;
 
           if(x<menor)
             menor=x;
 
           if(x>maior)
-            maior=x;   
+            maior=x;
          }
 
          return (maior-menor);
@@ -56,19 +56,19 @@ float sensorCorrente::calculaRMS(){
        float periodo= (float) 1/60;
        float soma=0;
        float M;
-       int N;    
        unsigned long inicio = millis();
 
         for( N=0 ; millis()-inicio < periodo; N++){
              M=analogRead(pino)-zero;
-             soma += M*M;    
+             soma += M*M;
         }
         return sqrt(soma/N);
-      
+
+
 }
 float sensorCorrente::calculaCorrente(int A){
-      
-    
+
+
        A = (A*(5/1024))/fatorConversao;
 
        return A;
