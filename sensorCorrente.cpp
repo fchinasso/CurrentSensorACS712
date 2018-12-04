d820ccc287b0683a385295e646b90cef0a2e22b3
Felipe Chinasso <45355530+fchinasso@users.noreply.github.com>
2018-12-04 08:55:27 -0200

    changes to "calibrar" function

--- sensorCorrente.cpp
+++ sensorCorrente.cpp
@@ -34,27 +34,16 @@ void sensorCorrente::calibrar(){
 
 
 
-       // Read 1.1V reference against AVcc
-       // set the reference to Vcc and the measurement to the internal 1.1V reference
-       #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-         ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-       #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-          ADMUX = _BV(MUX5) | _BV(MUX0) ;
-       #else
-         ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-       #endif
-
-       delay(2); // Wait for Vref to settle
-       ADCSRA |= _BV(ADSC); // Start conversion
-       while (bit_is_set(ADCSRA,ADSC)); // measuring
-
-       uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
-       uint8_t high = ADCH; // unlocks both
-
-       long result = (high<<8) | low;
-
-       result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
-       trueVCC = result; // Vcc in millivolts
+     long result;
+// Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1125300L / result; // Back-calculate AVcc in mV
+  trueVCC = result;
 
 
 }
