929adaec4842f66f9b58981849a983a86dcaa3fa
Felipe Chinasso <45355530+fchinasso@users.noreply.github.com>
2019-01-09 10:40:03 -0200

    changes to "test"

--- teste/teste.ino
+++ teste/teste.ino
@@ -6,8 +6,11 @@ sensorCorrente s01(A3,T30A,'D','1'); //declarcao de objetos para cada fase
 sensorCorrente s02(A4,T30A,'D','2');
 sensorCorrente s03(A5,T30A,'D','3');
 
+
+
+
 int intervalo = 2; //intervalo entre  cada medição
-float tempo = 0.1; //tempo para calculo da média
+float tempo = 5; //tempo para calculo da média
 
 SoftwareSerial mySerial(10, 11); // Declaramos os pinos RX(10) e TX(11) que vamos a usar na comunicacao Serial
 
@@ -32,6 +35,8 @@ char r3[] = "CLOSE";
 
 void setup() {
 
+pinMode(9, OUTPUT);//Pino de reset
+digitalWrite(9, HIGH);
 Serial.begin(9600);
 mySerial.begin(9600);     // Iniciando comunicação serial
 delay(1000);
@@ -46,6 +51,7 @@ s03.printCalibrar();*/
 
 intervalo=intervalo*1000;
 tempo=tempo*1000*60;
+reset();
 
 
 }
@@ -83,7 +89,15 @@ float I3=0;
 
 
     flag = enviar(I1,I2,I3);
+      if(flag==true)
+        Serial.println(String("ENVIADO!"));
+      else{
+      Serial.println(String("***********ATENCAO********"));
+      Serial.println(String("******Falha no envio******"));
 }
+}
+
+
 bool enviar(float fase1,float fase2,float fase3) {
 
   int count = 0;
@@ -98,7 +112,7 @@ bool enviar(float fase1,float fase2,float fase3) {
   dtostrf(fase2, 7, 2,f3);
 
 
-  char * str = (char *) malloc(1 + strlen(s6) + 3*strlen(f1)+2 * strlen("&field2="));
+  char * str = (char *) malloc(1 + strlen(s6) + 3*strlen(f1) + 2*strlen("&field2="));
   snprintf(str,100,"%s%s%s%s%s%s",s6,f1,"&field2=",f2,"&field3=",f3);
 
 
@@ -109,12 +123,14 @@ bool enviar(float fase1,float fase2,float fase3) {
       flag = RotinaGSM(str);
       count++;
     }
-    if (count > 5)
+    if (count > 3){
+       free(str);
       return false;
-
-    if (flag == 1)
+    }
+    if (flag == 1){
+      free(str);
       return true;
-
+    }
   }
 }
 
@@ -124,62 +140,67 @@ int RotinaGSM(char valor[]) {
   int cont = 0;
 
 
-
   while (sendATcommand(s1, r1, 200) == 0)  // AT
   {
     sendATcommand(s1, r1, 200);
   }
+  delay(1000);
 
 
   while (sendATcommand(s8, r1, 200) == 0)  // CIPSHUT
   {
+    delay(1000);
     sendATcommand(s8, r1, 200);
-     cont++;
-    if (cont > 4)
-      return false ;
   }
-  cont=0;
-  delay(500);
+  delay(2000);
 
   while (sendATcommand(s3, r1, 200) == 0) //AT+CSTT=\"smart.m2m.vivo.com.br\",\"vivo\",\"vivo\"
   {
+    delay(1000);
     sendATcommand(s3, r1, 200);
   }
-  delay(1000);
+  delay(2000);
 
   while (sendATcommand(s8, r1, 200) == 0)  // CIPSHUT
   {
+    delay(1000);
     sendATcommand(s8, r1, 200);
   }
-  delay(500);
+  delay(2000);
 
-  while (sendATcommand(s4, r2, 2000) == 0) // AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80
+  while (sendATcommand(s4, r2, 5000) == 0) // AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80
   {
-    sendATcommand(s4, r2, 2000);
+    delay(1000);
+    sendATcommand(s4, r2, 5000);
     cont++;
-    if (cont > 4)
-      return false ;
+    if (cont > 4){
+    reset();
+      return 0 ;
+    }
   }
   cont = 0;
-  delay(1000);
+  delay(2000);
 
-  sendATcommand(s5, r1, 2000);      // AT+CIPSEND=51
+  sendATcommand(s5, r1, 2000);      // AT+CIPSEND=
   delay(2000);
 
   sendATcommand(valor, r1, 2000);      //GET /update?api_key=9EZ3TG966X160QBN&field1=corrente
-  delay(1000);
+  delay(2000);
 
   while (sendATcommand(s7, r3, 200) == 0) //AT+CIPCLOSE
   {
     sendATcommand(s7, r3, 200);
     cont++;
-    if (cont > 4)
-      return false;
+     if (cont > 4){
+       reset();
+      return 0 ;
+    }
   }
 
-  return true;
-}
+  return 1;
 
+
+}
 int8_t sendATcommand(char* ATcommand, char* expected_answer1, unsigned int timeout) {
   int i;
   uint8_t x = 0,  answer = 0;
@@ -215,3 +236,10 @@ int8_t sendATcommand(char* ATcommand, char* expected_answer1, unsigned int timeo
   Serial.println(response);
   return answer;
 }
+void reset() //Reinicia o GSM
+  {
+    digitalWrite(9, 0);
+    delay(2000);
+    digitalWrite(9, 1);
+
+  }
